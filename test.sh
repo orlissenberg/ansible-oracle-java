1994f3e1cbc468abf78084632b37ab3e024cc042
Onno Lissenberg <orlissenberg@gmail.com>
2015-06-26 03:35:19 -0700

    Add Travis support.

--- test.sh
+++ test.sh
@@ -2,19 +2,21 @@
 
 CURRENT_DIR=${PWD}
 TMP_DIR=/tmp/ansible-test
-
 mkdir -p $TMP_DIR 2> /dev/null
 
+# Create hosts inventory
 cat << EOF > $TMP_DIR/hosts
 [webservers]
 localhost ansible_connection=local
 EOF
 
+# Create group_vars for the webservers
 mkdir -p $TMP_DIR/group_vars 2> /dev/null
 cat << EOF > $TMP_DIR/group_vars/webservers
 
 EOF
 
+# Create Ansible config
 cat << EOF > $TMP_DIR/ansible.cfg
 [defaults]
 roles_path = $CURRENT_DIR/../
@@ -23,4 +25,14 @@ EOF
 
 export ANSIBLE_CONFIG=$TMP_DIR/ansible.cfg
 
+# Syntax check
+ansible-playbook playbook.yml -i $TMP_DIR/hosts --syntax-check
+
+# First run
 ansible-playbook playbook.yml -i $TMP_DIR/hosts
+
+# Idempotence test
+ansible-playbook playbook.yml -i $TMP_DIR/hosts | grep -q 'changed=0.*failed=0' \
+	&& (echo 'Idempotence test: pass' \
+	&& exit 0) || (echo 'Idempotence test: fail' && exit 1) \
+
