5121f1cfb7262992a2dc32af5d58849150350e64
O. Lissenberg <olissenberg@rsm.nl>
2017-06-30 21:28:59 +0200

    Make various improvements.

--- test.sh
+++ test.sh
@@ -2,37 +2,50 @@
 
 CURRENT_DIR=${PWD}
 TMP_DIR=/tmp/ansible-test
-mkdir -p $TMP_DIR 2> /dev/null
+mkdir -p ${TMP_DIR} 2> /dev/null
 
 # Create hosts inventory
-cat << EOF > $TMP_DIR/hosts
+cat << EOF > ${TMP_DIR}/hosts
 [webservers]
 localhost ansible_connection=local
 EOF
 
-# Create group_vars for the webservers
-mkdir -p $TMP_DIR/group_vars 2> /dev/null
-cat << EOF > $TMP_DIR/group_vars/webservers
+# Create group_vars for the web servers
+mkdir -p ${TMP_DIR}/group_vars 2> /dev/null
+cat << EOF > ${TMP_DIR}/group_vars/webservers
 
 EOF
 
 # Create Ansible config
-cat << EOF > $TMP_DIR/ansible.cfg
+cat << EOF > ${TMP_DIR}/ansible.cfg
 [defaults]
-roles_path = $CURRENT_DIR/../
-host_key_checking = False
+roles_path = ${CURRENT_DIR}/../
+host_key_checking = false
 EOF
 
-export ANSIBLE_CONFIG=$TMP_DIR/ansible.cfg
+export ANSIBLE_CONFIG=${TMP_DIR}/ansible.cfg
+
+# Create playbook.yml
+cat << EOF > ${TMP_DIR}/playbook.yml
+---
+
+- hosts: webservers
+  gather_facts: yes
+  become: yes
+
+  roles:
+    - ansible-oracle-java
+EOF
 
 # Syntax check
-ansible-playbook playbook.yml -i $TMP_DIR/hosts --syntax-check
+ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts --syntax-check
 
 # First run
-ansible-playbook playbook.yml -i $TMP_DIR/hosts
+ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts
 
 # Idempotence test
-ansible-playbook playbook.yml -i $TMP_DIR/hosts | grep -q 'changed=0.*failed=0' \
-	&& (echo 'Idempotence test: pass' \
-	&& exit 0) || (echo 'Idempotence test: fail' && exit 1) \
+ansible-playbook playbook.yml -i ${TMP_DIR}/hosts | grep -q 'changed=0.*failed=0' \
+  && (echo 'Idempotence test: pass' \
+  && exit 0) || (echo 'Idempotence test: fail' && exit 1) \
 
+javac -version
