1797f19bd0f8104e3d4a33aa27e4de81a5da6295
O. Lissenberg <olissenberg@rsm.nl>
2017-07-01 09:02:03 +0200

    Fix a missing file reference.

--- test.sh
+++ test.sh
@@ -44,7 +44,7 @@ ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts --syntax-check
 ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts
 
 # Idempotence test
-ansible-playbook playbook.yml -i ${TMP_DIR}/hosts | grep -q 'changed=0.*failed=0' \
+ansible-playbook ${TMP_DIR}/playbook.yml -i ${TMP_DIR}/hosts | grep -q 'changed=0.*failed=0' \
   && (echo 'Idempotence test: pass' \
   && exit 0) || (echo 'Idempotence test: fail' && exit 1) \
 
